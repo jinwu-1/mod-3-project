93120766709e4e8943a11938233a9e9127eacdef
wenglin1 <wenglin@buffalo.edu>
2020-03-02 16:54:47 -0500

    asdf

--- backend/app/controllers/cart_items_controller.rb
+++ backend/app/controllers/cart_items_controller.rb
@@ -1,2 +1,8 @@
 class CartItemsController < ApplicationController
+
+    def index
+        @cart_items = CartItem.all
+        render json: @cart_items
+    end
+
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # resources :cart_items
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index]
+  resources :cart_items, only: [:index, :show]
   # resources :carts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
